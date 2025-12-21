1f289da3556347e3bcd43dfc5f6b06d1728be81e
guopop <guopop1994@gmail.com>
2025-12-22 01:18:46 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -21,7 +21,9 @@ const { posts, rewrites } = await usePosts({
   pageSize: 6,
   homepage: true,
   srcDir: 'posts',
-  autoExcerpt: 150
+  autoExcerpt: 150,
+  prev: false,
+  next: false
 });
 
 export default defineConfig<ThemeConfig>({
@@ -35,27 +37,12 @@ export default defineConfig<ThemeConfig>({
     page: {
       max: 5
     },
-    outline: { level: 2 },
+    outline: false,
     nav: [
       { text: '首页', link: '/' },
-      { text: '文档', link: '/docs/doc1' },
       { text: '分类', link: '/category' },
       { text: '归档', link: '/archives' }
-    ],
-    sidebar: {
-      '/docs': [
-        {
-          text: '如何使用电饭煲',
-          items: [
-            { text: '选择合适的电饭煲', link: '/docs/doc1' },
-            { text: '煮出松软米饭的技巧', link: '/docs/doc2' },
-            { text: '电饭煲的多功能用途', link: '/docs/doc3' },
-            { text: '电饭煲的清洁与保养', link: '/docs/doc4' },
-            { text: '电饭煲常见问题处理', link: '/docs/doc5' }
-          ]
-        }
-      ]
-    }
+    ]
   },
   vite: {
     plugins: [RssPlugin(RSS)]
